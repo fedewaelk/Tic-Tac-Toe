02511aaf6894cac34c1d439b5c30cc797d3d377d
FedeWaelk <fedewaelk@gmail.com>
2025-01-08 11:11:06 -0300

    render the gameboard

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-const Gameboard = (() => {
-    let gameboard = ["", "", "", "", "", "", "", "", ""];
-    });
+const GameBoard = (() => {
+    let gameBoard = ["", "", "", "", "", "", "", "", ""];
+    const renderBoard = () => {
+        const board = document.getElementById("gameBoard");
+        gameBoard.forEach((mark, index) => {
+            const box = document.createElement("div");
+            box.classList.add("box");
+        })
+    }
+})();
