2ebb19d8f544241704dc48d06e02124bd6bb2765
FedeWaelk <fedewaelk@gmail.com>
2025-01-08 17:57:54 -0300

    set the start game

--- script.js
+++ script.js
@@ -44,7 +44,47 @@ document.addEventListener("DOMContentLoaded", () => {
     const checkWinner = () => {
         const board = gameModule.getBoard();
         const winLines = [
-            []
-        ]
-    }
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        for (line of winLines) {
+            const [a, b ,c] = line;
+            if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+                return board[a];
+            }
+        }
+
+        return board.includes("") ? null : "Tie"
+    };
+
+    const makeMove = (index) => {
+        if (!gameOn) return;
+        if (gameModule.getBoard()[index]) return;
+        
+        gameModule.updateBox(index, currentPlayer);
+        const winner = checkWinner();
+        if (winner) {
+            endGame(winner);
+        } else {
+            switchPlayer();
+        }
+    };
+
+    const startGame = () => {
+        const player1Name = document.getElementById("player1").value || "Player 1";
+        const player2Name = document.getElementById("player2").value || "Player 2";
+    
+        players = { X: player1Name, O: player2Name };
+        currentPlayer = 'X';
+        gameOn = true;
+        gameModule.resetBoard();
+        document.getElementById('result').value = `${players[currentPlayer]}'s Turn`;
+      };
   }
