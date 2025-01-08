d50f5a5a29388ff35254b44ee3f23a9c720415a0
FedeWaelk <fedewaelk@gmail.com>
2025-01-08 13:45:43 -0300

    render the gameboard in the game module IIFE

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-const GameBoard = (() => {
+const gameModule = (() => {
     let gameBoard = ["", "", "", "", "", "", "", "", ""];
+
     const renderBoard = () => {
         const board = document.getElementById("gameBoard");
+        board.innerHTML = "";
+
         gameBoard.forEach((mark, index) => {
             const box = document.createElement("div");
             box.classList.add("box");
-        })
-    }
+            if (mark) box.classList.add("taken");
+            box.textContent = mark;
+            box.addEventListener("click", () => console.log(`Clic on box ${index}`));
+            board.appendChild(box);
+        });
+    };
+
+    const updateBox = (index, mark) => {
+        if (gameBoard[index] === "") {
+            gameBoard[index] = mark;
+            renderBoard();
+        }
+    };
+
+    const getBoard = () => gameBoard;
+
+    return { renderBoard, updateBox, getBoard };
 })();
+
+document.addEventListener("DOMContentLoaded", () => {
+    gameModule.renderBoard();
+  });
+  
